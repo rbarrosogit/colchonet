ce649b0b3db0c6b4bcbc895ab33034dc3be5cb0f
Rodrigo Barroso <direitoreal@Brunos-Mac-mini.local>
2020-03-09 15:27:55 -0300

    commit de segunda

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,6 @@
 class Room < ApplicationRecord
-    #validates_presence_of :title, :location
-    #validates :title, presence: true
-    # validates :title, :location, :description, presence: true
     validates :title, :location, :description, presence: true
-    validates :description, length: { in: 5..20 }
+    validates :description, length: { minimum: 5 }
 
     def complete_name
       "#{title}, #{location}"
